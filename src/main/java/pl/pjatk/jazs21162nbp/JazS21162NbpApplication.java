c912112692015910eac8b443997664a5c4abcd37
s21162-pj <s21162@pjwstk.edu.pl>
2021-07-04 16:44:21 +0200

    Initial commit

---
+++ src/main/java/pl/pjatk/jazs21162nbp/JazS21162NbpApplication.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs21162nbp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JazS21162NbpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JazS21162NbpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pjatk/jazs21162nbp/JazS21162NbpApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pjatk.jazs21162nbp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JazS21162NbpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
